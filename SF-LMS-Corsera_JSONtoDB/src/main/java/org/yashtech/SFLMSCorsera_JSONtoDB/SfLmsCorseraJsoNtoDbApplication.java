7a7814f2d52891df5c7b81b2302c4eca7f85ba14
ishatiwari26 <isha.tiwari@yash.com>
2019-03-18 15:12:38 +0530

    SF-LMS-Coursera

---
+++ SF-LMS-Corsera_JSONtoDB/src/main/java/org/yashtech/SFLMSCorsera_JSONtoDB/SfLmsCorseraJsoNtoDbApplication.java
@@ -0,0 +1,13 @@
+package org.yashtech.SFLMSCorsera_JSONtoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfLmsCorseraJsoNtoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfLmsCorseraJsoNtoDbApplication.class, args);
+	}
+
+}
